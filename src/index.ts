0e701faa9b32138c81bb35aed30b7e2fcee3619d
Nikhil S <nikhilsnayak3473@gmail.com>
2024-09-11 09:50:56 +0530

    chore: export types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { types } from "./constants";
+import { modifiers, types } from "./constants";
 
 export const CodeSyntacticSugar = {
   TokenTypes: types,
   TokenMap: new Map(types.map((type, i) => [type, i])),
+  LineModifiers: modifiers,
 } as const;
 
+export * from "./types";
 export * from "./tokenize";
 export * from "./generate";
 export * from "./to-html";
